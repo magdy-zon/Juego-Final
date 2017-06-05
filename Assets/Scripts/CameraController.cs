a10e20ead48ebb8ed03985ac26eaff92f995cada
Magdiel Juárez Guerrero <mJuarez@vl23h67.fciencias.unam.mx>
2017-06-05 14:34:24 -0500

    Creado commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	/**
+	 * Controller to indicate the target of the camera
+	 */
+	public Transform targetCamera;
+	public Vector3 distance = new Vector3 (0f, 3f, -15f);
+	public float positionDamping = 2f; 
+	public float rotationDamping = 2f;
+
+	private Transform thisTransform;
+
+	void Start () {
+		thisTransform = GetComponent<Transform> ();
+	}
+
+	void LateUpdate () {
+
+		if (targetCamera == null)
+			return;
+
+		Vector3 wantedPosition = targetCamera.position + (targetCamera.rotation * distance);
+		Vector3 currentPosition = Vector3.Lerp (thisTransform.position, wantedPosition, positionDamping * Time.deltaTime);
+
+		thisTransform.position = currentPosition;
+		Quaternion wantedRotation = Quaternion.LookRotation (targetCamera.position - thisTransform.position, targetCamera.up);
+
+		thisTransform.rotation = wantedRotation;
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	/**
+	 * Controller for set the movement of the player
+	 */
+	 public float inputDelay = 0.1f;
+	public float forwardVel = 12f;
+	public float rotateVel = 160f;
+
+	Rigidbody rbody;
+	Quaternion targetRotation;
+	Vector3 walking;
+	float forwardInput, turnInput;
+
+	void Start () {
+		targetRotation = transform.rotation;
+		if(GetComponent<Rigidbody> ())
+			rbody = GetComponent<Rigidbody> ();
+		else
+			Debug.LogError("Falle");
+
+		forwardInput = turnInput = 0;
+	}
+
+	void GetInput() {
+		turnInput = Input.GetAxis ("Horizontal");
+		forwardInput = Input.GetAxis ("Vertical");
+	}
+
+	void Turn () {
+		if(Mathf.Abs(turnInput) > inputDelay)
+			targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		transform.rotation = targetRotation;
+	}
+
+	void Update () {
+		GetInput();
+		Turn();
+	}
+
+	void Run () {
+		if(Mathf.Abs(forwardInput) > inputDelay)
+			rbody.velocity = transform.forward * forwardInput * forwardVel;
+		else
+			rbody.velocity = Vector3.zero;
+	}
+
+	void FixedUpdate () {
+		Run();
+	}
+	
+}
