91667fddd0b37e4b7b753c5fc05794ddc5b29da6
Magdiel Juárez Guerrero <mJuarez@Magdiels-MacBook-Pro.local>
2017-06-15 17:22:24 -0500

    para hacer merge

---
+++ Assets/Scripts/ScenaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenaController : MonoBehaviour {
+
+	public float rotateVel = 160f;
+
+	Rigidbody rbody;
+	Quaternion targetRotation;
+
+	// Use this for initialization
+	void Start () {
+		targetRotation = transform.rotation;
+		if(GetComponent<Rigidbody> ())
+			rbody = GetComponent<Rigidbody> ();
+		else
+			Debug.LogError("Falle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotarHistoria ();
+	}
+
+	void rotarHistoria() {
+		targetRotation *= Quaternion.AngleAxis(rotateVel * Time.deltaTime, Vector3.up);
+		transform.rotation = targetRotation;
+	}
+}
