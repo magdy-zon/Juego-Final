4625bc9db4b04da51cdecae51f33befa0b2474fd
Magdiel Juárez Guerrero <mJuarez@Magdiels-MacBook-Pro.local>
2017-06-16 10:45:54 -0500

    para entregar

---
+++ Assets/Scripts/InicioController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InicioController : MonoBehaviour {
+
+	public GameObject godRays;
+	public GameObject itemHistory;
+	public GameObject jaulaCirco;
+	public GameObject cañon;
+	public GameObject aCarpaGrande;
+
+	/**
+	 * Script for do actions when the gamer collides when a specific target
+	 */
+	void Start () {
+
+		aCarpaGrande = GameObject.FindGameObjectWithTag ("aCarpaGrande");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "aCarpaGrande") {
+		
+		}
+	}
+
+
+	void cuentaHistoriaMago() {
+		Debug.Log("Contando la historia...");
+	}
+
+	void activaHistoria() {
+		itemHistory.SetActive (true);
+		godRays.SetActive (true);
+		if (jaulaCirco != null)
+			jaulaCirco.SetActive (false);
+		if (cañon != null)
+			cañon.SetActive (false);
+		if (aCarpaGrande != null)
+			SceneManager.LoadScene ("carpaGrandeJuego");
+	}
+
+	void recargaEscenaCircoPrincipal() {
+
+
+	}
+}
---
+++ Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour {
+
+	/**
+	 * Controller for set the movement of the player
+	 */
+	public float inputDelay = 0.1f;
+	public float forwardVel = 12f;
+	public float rotateVel = 160f;
+	public float distanceToGround = 0.1f;
+	public float jumpVel = 12f;
+	public float downAcccel = 0.65f;
+
+	public LayerMask ground; 
+
+	Rigidbody rbody;
+	Quaternion targetRotation;
+	Vector3 velocity = Vector3.zero;
+	float forwardInput, turnInput, jumpInput;
+
+	void Start () {
+		targetRotation = transform.rotation;
+		if(GetComponent<Rigidbody> ())
+			rbody = GetComponent<Rigidbody> ();
+		else
+			Debug.LogError("Falle");
+
+		forwardInput = turnInput = jumpInput = 0;
+	}
+
+	void GetInput() {
+		turnInput = Input.GetAxis ("Horizontal");
+		forwardInput = Input.GetAxis ("Vertical");
+		jumpInput = Input.GetAxisRaw ("Jump");
+	}
+
+	void Turn () {
+		if(Mathf.Abs(turnInput) > inputDelay)
+			targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+		transform.rotation = targetRotation;
+	}
+
+	bool Grounded () {
+		return Physics.Raycast( transform.position, Vector3.down, distanceToGround, ground);
+	}
+
+	void Update () {
+		GetInput();
+		Turn();
+	}
+
+	void Run () {
+		if(Mathf.Abs(forwardInput) > inputDelay)
+			velocity.z = forwardInput * forwardVel;
+		else
+			velocity.z = 0;
+	}
+
+	void Jump () {
+		if (jumpInput > 0 && Grounded ())
+			velocity.y = jumpVel;
+		else if (jumpInput == 0 && Grounded ())
+			velocity.y = 0;
+		else
+			velocity.y -= downAcccel;	
+	}
+
+	void FixedUpdate () {
+		Run();
+		Jump ();
+
+		rbody.velocity = transform.TransformDirection (velocity);
+
+	}
+
+}
