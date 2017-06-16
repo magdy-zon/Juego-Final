bb74a432f5d466a331a0b45e86e9fb123c6a0693
xhanzary <xhanzary@ciencias.unam.mx>
2017-06-16 10:50:19 -0500

    escena globos

---
+++ Assets/Scripts/Globos/Disparar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour {
+
+	public GameObject disparo;
+	public Transform posicion;
+	public float tiempoFuego;
+	public float tiempoEspera;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("fuego",tiempoFuego,tiempoEspera);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void fuego(){
+		Instantiate (disparo, posicion.position, posicion.rotation);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("jugador")) {
+			Destroy (other.gameObject);
+			print ("hola");
+		} else {
+			print (other.gameObject.name);
+		}
+	}
+}
---
+++ Assets/Scripts/Globos/Rayo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rayo : MonoBehaviour {
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(transform.forward * 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
